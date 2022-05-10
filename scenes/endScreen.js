fe04b670eb5b71e98430b95214d5a165e101897a
Starbird-Studios <64431622+StarbirdTech@users.noreply.github.com>
2022-05-10 13:48:09 -0400

    graph WIP

--- scenes/endScreen.js
+++ scenes/endScreen.js
@@ -1,10 +1,14 @@
 function endScreen() {
+  let graph;
+  let data = [10,10];
   this.enter = function() {
-    background('#424549');
+    graph = new BarGraph(10,10,data);
+    //background('#424549');
+    background(255);
     textSize(75);
     textAlign(CENTER);
     fill('#EF2E72');
-    text("End Screen", width / 2, height/2);
+    text("End Screen", width / 2, height/3);
     /*httpPost('https://sheet2api.com/v1/OL0isnynQyCu/test/Sheet1', 'json',
     {
       "Level 1": lv1,
@@ -12,5 +16,20 @@ function endScreen() {
       "Level 3": lv3,
       "Level 4": lv4
     });*/
+    graph.draw();
+  }
+}
+
+class BarGraph {
+  constructor(x, y, data) {
+    this.x = x;
+    this.y = y;
+    this.height = height/data.length;
+    this.data = data;
+  }
+  draw() {
+    for (let i = 0; i < this.data.length; i++) {
+      rect(this.x,this.y+this.height*i,map(this.data[i],0,100,this.x,width-this.x*2),this.height);
+    }
   }
 }
