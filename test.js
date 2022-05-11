c43123eec189d6289d1330b07682104fc2cffe4e
nickthorn402 <98910010+nickthorn402@users.noreply.github.com>
2022-05-11 13:26:31 -0400

    TEST

--- test.js
+++ test.js
@@ -1,34 +1,12 @@
-let data = {
-    grid: {rand: [0,0,0,0,0,0,0,0,0], human: [0,0,0,0,0,0,0,0,0]},
-    button: {rand: [], human: []},
-    slider: {rand: [], human: []},
-    colorGrid: {rand: [], human: []},
-  }
-
-class BarGraph{
-    constructor(data){
-        this.data = data;
-    }
-
-    setup(){
-        textAlign(CENTER);
-        textSize(50);
-        for(let i = 0; i < 100; i++){
-            data.grid.rand[floor(random(9))]++;
-        }
-    }
-    draw(){
-        for(let i = 0; i < 9; i++){
-            fill(255);
-            rect(i*66+11, 550, 50, -map(data.grid.rand[i], 0, 100, 0, height*4), 5);
-            fill(0);
-            text(data.grid.rand[i], i*66+35, 550-data.grid.rand[i]*12);
-        }
-    }
-}
-
 function setup() {
     createCanvas(600, 600);
-    textAlign(CENTER);
-    BarGraph = new BarGraph(data);
+    textSize(30)
 }
+function draw() {
+    text('Pressed', 50, height/2+150);
+    text('Skipped', 440, height/2+150);
+    fill(255);
+    rect(50, height/2, 250, 50);
+    fill(0);
+    rect(550, height/2, -250, 50);
+} 
